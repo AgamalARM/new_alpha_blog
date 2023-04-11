44b4b5f104341094715032312c0192641e0218b1
AgamalARM <agamalarm@gmail.com>
2023-04-11 12:46:13 +0000

    add create new acrtion

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,4 +7,20 @@ class ArticlesController < ApplicationController
     def show
         @article = Article.find(params[:id])
     end
+    
+    def new
+        
+    end
+    
+    def create
+        @article = Article.new(params.require(:article).permit(:title,:description))
+        #render plain: @article.inspect
+        @article.save
+        redirect_to article_path(@article)
+        #redirect_to @article  -----OR
+    
+    end
+    
+    
+    
 end
