ea09620177b7c602e8a4ef59b0155901e0c9b88f
AgamalARM <agamalarm@gmail.com>
2023-07-04 10:45:43 +0000

    work done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,8 @@
 class PagesController < ApplicationController
     def home
     end
+    
+    def about
+        @ml = `python3 lib/assets/python/hello.py`
+    end
 end
---
+++ lib/assets/python/hello.py
@@ -0,0 +1,7 @@
+print ("This is python code")
+
+import numpy as np
+
+a = np.array([1,2,3])
+
+print(a)
